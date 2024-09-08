0ed5e5dce8407d2f9b38a1aa46d0da196b29a9d2
iishu <ishikasrivastava27594@gmail.com>
2024-09-08 16:03:28 +0530

    add parent element

--- ToDo List/index.js
+++ ToDo List/index.js
@@ -14,3 +14,13 @@ function addTask(){
     }
     inputBox.value="";
 }
+
+listContainer.addEventListener("click",function (e) {
+    if(e.target.tagName==="LI"){
+        e.targett.classList.toggle("checked");
+
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
