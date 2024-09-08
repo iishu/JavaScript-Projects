93ceb2e08d2f5e7dfd82e77a0ad2f4fe0dd524a7
iishu <ishikasrivastava27594@gmail.com>
2024-09-08 16:22:37 +0530

    final commit

--- ToDo List/index.js
+++ ToDo List/index.js
@@ -9,27 +9,30 @@ function addTask(){
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
         let span=document.createElement("span");
-        span.innerHTML="\u00d7";
+        span.innerHTML="\u00d7"; // Adds the close button (×) to the list item
         li.appendChild(span);
     }
-    inputBox.value="";
-    saveData();
+    inputBox.value="";  // Clears the input box after adding a task
+    saveData(); // Saves the updated list to localStorage
 }
 
 listContainer.addEventListener("click",function (e) {
     if(e.target.tagName==="LI"){
-        e.target.classList.toggle("checked");
+        e.target.classList.toggle("checked");  // Toggles the "checked" class for list items
+        saveData(); // Save changes (in case the item is checked or unchecked)
+
 
     }
     else if(e.target.tagName==="SPAN"){
-        e.target.parentElement.remove();
+        e.target.parentElement.remove();  // Removes the list item if the close button is clicked
+        saveData();  // Save changes after removing the item
     }
 },false);
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML); //this will store the list data in local storage
 }
-function showTasks(){
+function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");// this will store all the list data in the name of data
 }
-showTasks();
+showTask();
