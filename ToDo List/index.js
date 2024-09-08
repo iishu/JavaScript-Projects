9bde855ca7490112b9ab117f1159fdf990c9add2
iishu <ishikasrivastava27594@gmail.com>
2024-09-08 16:00:02 +0530

    added cross functionalities

--- ToDo List/index.js
+++ ToDo List/index.js
@@ -2,11 +2,15 @@ const inputBox=document.getElementById("input-box");
 const listContainer=document.getElementById("list-container");
 function addTask(){
     if(inputBox.value===''){
-        alert("You must write something!");
+        alert("You must write something!"); //if the input box is empty it will give this 
     }
     else{
         let li=document.createElement("li");
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value="";
 }
